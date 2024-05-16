5efa9302b750c9dfe8caa38a4bde4a24b0b6361a
Eddy Chen <eddychen615@gmail.com>
2024-05-16 11:30:23 +0900

    layout adjust.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,21 @@ function App() {
         <p className="dateTitle">2024年06月01日</p>
         <Header link="/Receive" linkName="今日の明細" />
       </div>
+      <div className="switchInput">
+        <button className="switchBtn pressed">支出</button>
+        <button className="switchBtn">収入</button>
+      </div>
+      <div className="inputArea">
+        <div className="inputbox">
+          <label>項目</label>
+          <input type="text" />
+        </div>
+        <div className="inputbox">
+          <label>金額</label>
+          <input type="number" />
+        </div>
+        <input type="submit" />
+      </div>
       <h1>APP</h1>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import './Header.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+// eslint-disable-next-line react/prop-types
 function Header({ linkName, link }) {
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    // 添加短暂延迟，以确保动画效果显示
+    setTimeout(() => {
+      navigate(link);
+    }, 300); // 延迟时间与动画时间一致
+  };
+
   return (
     <div>
       <div className="header">
-        <Link to={link} className="headerBtn">
+        <Link to={link} className="headerBtn" onClick={handleClick}>
           {linkName}
         </Link>
       </div>
