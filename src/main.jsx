0b416ec469c3015e8f8c93063b06b9cffcf4adbd
Eddy Chen <eddychen615@gmail.com>
2024-05-29 13:50:27 +0900

    try

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { useEffect } from 'react';
 
-//service worker register
 const registerServiceWorker = async () => {
-  if ('serviceworker' in navigator) {
+  if ('serviceWorker' in navigator) {
     try {
-      const registration = await navigator.serviceworker.register(
+      const registration = await navigator.serviceWorker.register(
         '/serviceworker.js',
         {
           scope: '/',
@@ -17,7 +15,7 @@ const registerServiceWorker = async () => {
       if (registration.installing) {
         console.log('正在安装 Service worker');
       } else if (registration.waiting) {
-        console.log('已安装 Service worker installed');
+        console.log('已安装 Service worker');
       } else if (registration.active) {
         console.log('啟動 Service worker');
       }
@@ -28,27 +26,6 @@ const registerServiceWorker = async () => {
 };
 
 registerServiceWorker();
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // 防止自動顯示安裝提示
-  e.preventDefault();
-  deferredPrompt = e;
-
-  // 在你認為合適的時候顯示提示
-  document.getElementById('installButton').addEventListener('click', (e) => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
-});
 
 const InstallPromptButton = () => {
   useEffect(() => {
