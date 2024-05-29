8ec6c878276b463cc59e06ee340e7c8f6d5914f4
Eddy Chen <eddychen615@gmail.com>
2024-05-29 13:14:43 +0900

    once

--- serviceworker.js
+++ serviceworker.js
@@ -1,46 +1,46 @@
-// const CACHE_NAME = 'my-cache-v1';
-// const urlsToCache = [
-//   '/index.html',
-//   '/src/main.jsx',
-//   '/src/index.css',
-//   '/src/App.jsx',
-//   '/src/App.css',
-// ];
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/src/main.jsx',
+  '/src/index.css',
+  '/src/App.jsx',
+  '/src/App.css',
+];
 
-// // 安裝階段 - 緩存資源
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       console.log('Opened cache');
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
+// 安裝階段 - 緩存資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-// // 激活階段 - 清理舊緩存
-// self.addEventListener('activate', (event) => {
-//   const cacheWhitelist = [CACHE_NAME];
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (cacheWhitelist.indexOf(cacheName) === -1) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
+// 激活階段 - 清理舊緩存
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
-// // 攔截網絡請求 - 提供緩存資源
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
+// 攔截網絡請求 - 提供緩存資源
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
