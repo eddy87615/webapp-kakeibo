1542368bbd9b88ea7fcac1d9aea3202a453624a3
Eddy Chen <eddychen615@gmail.com>
2024-05-29 14:04:22 +0900

    try

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Service worker register
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
