af5743ab79daf91b0cf1c20b1df68fa354811657
Eddy Chen <eddychen615@gmail.com>
2024-06-12 16:53:18 +0900

    logo route change.

--- src/components/OpenPage.jsx
+++ src/components/OpenPage.jsx
@@ -7,7 +7,7 @@ export default function OpenPage() {
         <div className="left"></div>
         <div className="right"></div>
         <div className="openimg">
-          <img src="/src/assets/Group 2.svg" alt="logo in starting page" />
+          <img src="/assets/Group 2.svg" alt="logo in starting page" />
           <h1>GrandBook</h1>
           <p>収支を簡単に記録！</p>
         </div>
